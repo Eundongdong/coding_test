4df9fb22c6d35b68c1474a34d201b64a462be79b
Eundongdong <51512016+Eundongdong@users.noreply.github.com>
2021-09-11 22:55:59 +0900

    파이썬 아주 기초 사칙연산

---
+++ 1000.py
@@ -0,0 +1,5 @@
+#split() : 입력 받은 값을 공백을 기준으로 분리
+# map (적용시킬 함수, 적용할 값)
+a, b = map(int, input().split())
+
+print(a * b)
---
+++ 10869.py
@@ -0,0 +1,8 @@
+a, b = map(int, input().split())
+c = int(a / b)
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(c)
+print(a % b)
