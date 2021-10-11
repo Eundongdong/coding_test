b3466ba859c427e15f49c80961c48716f5d655ad
Eundongdong <51512016+Eundongdong@users.noreply.github.com>
2021-10-11 23:04:30 +0900

    2Q

--- array/10818.py
+++ array/10818.py
@@ -1,4 +1,4 @@
 a = int(input())
 b = list(map(int,input().split()))
 
-print(min[b], max[b])
+print(min(b), max(b))
--- while/1110.py
+++ while/1110.py
@@ -1,16 +1,14 @@
-a = int(input())
-z = 0
-i = 0
-while(1):    
-    b = a /10
-    c = a % 10
-    x = b+c
-    y = c+(x%10)
-    z = (x%10) + (y%10)
-    if (a == z):
-        break
-    else:
-        i = i + 1
-        a = z
+n = int(input())
+num = n
+cnt = 0
 
-print(i)
+while True:
+    a = num //10
+    b = num %10
+    c = (a+b) %10
+    num = (b * 10) + c
+    
+    cnt = cnt + 1
+    if(num == n):
+        break
+print(cnt)
