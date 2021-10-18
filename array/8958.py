d5e06f3cc53c867d9dabd432f11df806fec70929
dongdong <jkjk4373@gmail.com>
2021-10-18 23:28:04 +0900

    Update 8958.py

--- array/8958.py
+++ array/8958.py
@@ -1,4 +1,13 @@
-num = int(input())
-for i in range(num):
-    a = map(int,input().split()) //어떻게 하나 단위로 읽을 수 있게 할 건지 고민해 봐야함
-    gggggggggggg
+a = int(input())
+for i in range(a):
+    b = input()
+    s = list(b)
+    sum = 0
+    c = 1
+    for i in s:
+        if i == 'O':
+            sum += c
+            c += 1
+        else:
+            c = 1
+    print(sum)
