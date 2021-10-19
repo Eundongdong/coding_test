ffc3e1c203d24308b332b91b8d1f2b2164547d38
dongdong <jkjk4373@gmail.com>
2021-10-19 23:36:30 +0900

    Update 8958.py

--- array/8958.py
+++ array/8958.py
@@ -1,12 +1,12 @@
-a = int(input())
-for i in range(a):
-    b = input()
-    s = list(b)
-    sum = 0
-    c = 1
+a = int(input()) #몇 개가 올 지 갯수 새어주는 변수 a
+for i in range(a):  #변수 만큼반복
+    b = input() #한번에 input을 받고
+    s = list(b) #이것을 그냥 바로 리스트에 넣음
+    sum = 0 #최종 print할 변수
+    c = 1 #연속된 O인지 확인하기 위한 변수
     for i in s:
         if i == 'O':
-            sum += c
+            sum += c 
             c += 1
         else:
             c = 1
