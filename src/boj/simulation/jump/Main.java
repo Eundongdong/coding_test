c829dacdc74bd1a7203f20ffaf479149567a0c37
[username] <51512016+Eundongdong@users.noreply.github.com>
2024-09-19 02:36:01 +0900

    모두 해결

--- src/boj/simulation/jump/Main.java
+++ src/boj/simulation/jump/Main.java
@@ -15,17 +15,17 @@ public class Main {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        int[][] dp = new int[n][n];
-        for(int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], 0);
-        }
+        long[][] dp = new long[n][n];
+//        for(int i = 0; i < n; i++) {
+//            Arrays.fill(dp[i], 0);
+//        }
         dp[0][0] = 1;
         //각자 칸에서 오,아래만큼 이동한 위치에 자기 dp값 더하기
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
                 if(dp[i][j] == 0) {continue;}
                 int go = map[i][j];
-                if(go == 0) {break;}
+                if(go == 0) {continue;}
                 if(i+go <n){
                     dp[i+go][j] += dp[i][j];
                 }
